227e0cd71acdea2279425f2fc33afcdb4d5de8d0
Sebastian Law <44045823+SebbyLaw@users.noreply.github.com>
2021-03-09 04:25:33 -0800

    implement CompassPOV

---
+++ src/main/java/org/bobabots253/CompassPOV.java
@@ -0,0 +1,31 @@
+package org.bobabots253;
+
+/**
+ * An enum that represents POV in cardinal directions.
+ *
+ * @author Sebastian Law
+ */
+public enum CompassPOV {
+    NORTH,
+    NORTHEAST,
+    EAST,
+    SOUTHEAST,
+    SOUTH,
+    SOUTHWEST,
+    WEST,
+    NORTHWEST,
+    NONE;
+    
+    /**
+     * Get the angle in degrees of a POV.
+     *
+     * The POV angles start at 0 with {@link #NORTH},
+     * and increase clockwise (eg {@link #EAST} is 90, {@link #NORTHWEST} is 315).
+     *
+     * @return The angle of the POV in degrees, or -1 if {@link #NONE}.
+     */
+    public int getAngle() {
+        if (this == NONE) return -1;
+        return this.ordinal() * 45;
+    }
+}
