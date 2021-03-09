4d03141fc05f6cc21afc80c52d8c79391cf5f122
Sebastian Law <44045823+SebbyLaw@users.noreply.github.com>
2021-03-09 03:37:05 -0800

    remove unnecessary arguments

--- src/test/java/TapiocaTestRunner.java
+++ src/test/java/TapiocaTestRunner.java
@@ -5,10 +5,7 @@ import org.junit.runners.Suite;
 import static org.junit.Assert.*;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({
-        /* Add inner classes here */
-        TapiocaTestRunner.ExampleTestClass.class,
-})
+@Suite.SuiteClasses({})
 public class TapiocaTestRunner {
     public static class ExampleTestClass {
         @Test
