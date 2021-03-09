165ff4c1edd0e0eba17db40f638648a0f37ab5ef
Sebastian Law <44045823+SebbyLaw@users.noreply.github.com>
2021-03-09 03:41:10 -0800

    add unit tests for deadband util

--- src/test/java/TapiocaTestRunner.java
+++ src/test/java/TapiocaTestRunner.java
@@ -1,3 +1,4 @@
+import org.bobabots253.Tapioca.utils.Calc;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -7,6 +8,26 @@ import static org.junit.Assert.*;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({})
 public class TapiocaTestRunner {
+    public static class UtilsTest {
+        public static class CalcTest {
+            @Test
+            public void testDeadband() {
+                final double deadband = 0.25;
+                
+                // deadband to zero
+                assertEquals(0, Calc.applyDeadband(0.1, deadband), 0);
+                assertEquals(0, Calc.applyDeadband(0.2, deadband), 0);
+                
+                // scale linearly
+                assertEquals(0.3, Calc.applyDeadband(0.5, deadband), 0.1);
+                assertEquals(0.06, Calc.applyDeadband(0.3, deadband), 0.01);
+                
+                // stay at 1
+                assertEquals(1, Calc.applyDeadband(1, deadband), 0);
+            }
+        }
+    }
+    
     public static class ExampleTestClass {
         @Test
         public void exampleTestMethod() {
