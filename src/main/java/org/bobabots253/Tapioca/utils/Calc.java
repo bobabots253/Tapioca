3f518e94259b3e5b7ed40a59570bd86f0ec416f6
Sebastian Law <44045823+SebbyLaw@users.noreply.github.com>
2021-03-09 03:40:13 -0800

    add deadband util

---
+++ src/main/java/org/bobabots253/Tapioca/utils/Calc.java
@@ -0,0 +1,24 @@
+package org.bobabots253.Tapioca.utils;
+
+public class Calc {
+    // Calc should not be instantiated
+    private Calc() {}
+    
+    /**
+     * Deadbands an input to [-1, -deadband], [deadband, 1], rescaling inputs to be
+     * linear from (deadband, 0) to (1,1)
+     *
+     * @param input    The input value to rescale
+     * @param deadband The deadband
+     * @return the input rescaled and to fit [-1, -deadband], [deadband, 1]
+     */
+    public static double applyDeadband(double input, double deadband) {
+        if (Math.abs(input) <= deadband) {
+            return 0;
+        } else if (Math.abs(input) == 1) {
+            return input;
+        } else {
+            return (1 / (1 - deadband) * (input + Math.signum(-input) * deadband));
+        }
+    }
+}
