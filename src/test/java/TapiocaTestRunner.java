e23ab2566e6f849418608c580f37c95bda5888e8
Sebastian Law <44045823+SebbyLaw@users.noreply.github.com>
2021-03-10 00:07:34 -0800

    move deadband unit tests

--- src/test/java/TapiocaTestRunner.java
+++ src/test/java/TapiocaTestRunner.java
@@ -1,44 +1,8 @@
-import org.bobabots253.Tapioca.utils.Deadband;
-import org.bobabots253.Tapioca.utils.LinearDeadband;
 import org.junit.Test;
-
-import java.util.Random;
 
 import static org.junit.Assert.*;
 
 public class TapiocaTestRunner {
-    public static class UtilsTest {
-        public static class DeadbandTest {
-            @Test
-            public void testDummyDeadband() {
-                Deadband dummy = Deadband.dummy();
-                Random random = new Random();
-                
-                for (int i = 0; i < 100; i++) {
-                    double val = random.nextDouble();
-                    assertEquals(dummy.calculate(val), val, 0);
-                }
-            }
-            
-            @Test
-            public void testLinearDeadband() {
-                final Deadband deadband = new LinearDeadband();
-                deadband.setDeadband(0.25);
-                
-                // deadband to zero
-                assertEquals(0, deadband.calculate(0.1), 0);
-                assertEquals(0, deadband.calculate(0.2), 0);
-                
-                // scale linearly
-                assertEquals(0.3, deadband.calculate(0.5), 0.1);
-                assertEquals(0.06, deadband.calculate(0.3), 0.01);
-                
-                // stay at 1
-                assertEquals(1, deadband.calculate(1), 0);
-            }
-        }
-    }
-    
     public static class ExampleTestClass {
         @Test
         public void exampleTestMethod() {
